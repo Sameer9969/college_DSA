2a7c3686ca0f80287ed0281c05b5311add3e21fb
Sameer9969 <thakursameer.singhrajput@gmail.com>
2026-08-17 15:03:35 +0530

    prime nummber

---
+++ p1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class p1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+        if(isprime(n)){
+            System.out.println(n+ "is prime");
+
+
+        }
+        else{
+            System.out.println(n+ "is not prime");
+        }
+    }
+    public static boolean isprime(long  n){
+        for (int i = 2;i<=Math.sqrt(n);i++){
+            if(n%i == 0)
+            {
+                return false;
+
+            }
+        }
+        return true;
+    }
+}
